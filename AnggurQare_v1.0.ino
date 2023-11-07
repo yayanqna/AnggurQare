6d6782cb980388edb5d079cc6a434016d120cc87
Selfrin Surya Maulana <selfr@DESKTOP-ERU73ID.(none)>
2023-11-07 14:50:13 +0700

    AnggurQare_v1.0.ino

--- AnggurQare_v1.0.ino
+++ AnggurQare_v1.0.ino
@@ -1,18 +1,27 @@
+#include <EEPROM.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "BH1750.h"
+#include "GravityTDS.h"
 #include "DHT.h"
-#include "Wire.h"
 #include "ModbusRtu.h"
-#include "SPI.h"
 
 #define soilPin A0
+#define pHPin A1
+#define tdsPin A2
 #define relayPump1 2
 #define relayPump2 7
 #define relayMist 8
+#define waterValve 3
+#define nutritionValve 4
 #define echoPin 5
 #define triggerPin 6
 #define dhtPin 9
 #define maxDistance 200
 
 DHT myDHT(dhtPin, DHT22);
+GravityTDS myTDS;
+BH1750 luxMeter(0x23);
 
 int soilPercentage;
 int readSoil;
@@ -20,39 +29,69 @@ int waterLevel;
 float temperature;
 float humidity;
 bool waterState = true;
+int readPH;
+float soilPH;
+float tdsValue;
+float readLux;
+int Distance = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   myDHT.begin();
+  Wire.begin();
+  luxMeter.begin();
   Serial.println("Machine ON");
   pinMode(relayPump1, OUTPUT);
   pinMode(relayPump2, OUTPUT);
   pinMode(relayMist, OUTPUT);
+  pinMode(waterValve, OUTPUT);
+  pinMode(nutritionValve, OUTPUT);
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT); //or use INPUT_PULLUP if trouble
+  myTDS.setPin(tdsPin);
+  myTDS.setAref(5.0);
+  myTDS.setAdcRange(1024);
+  myTDS.begin();
   digitalWrite(triggerPin, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
   //WaterPump for Tank Set-Up
-  int Distance = 0;
   Distance = getDistance();
   waterLevel = map(Distance, 20, 5, 0, 100);
   if (waterLevel < 100 && waterState == true)
     {
+      digitalWrite(waterValve, HIGH);
       digitalWrite(relayPump2, HIGH);
     }
   else
     {
       digitalWrite(relayPump2, LOW);
+      digitalWrite(waterValve, LOW);
       waterState = false;
 
       if (waterLevel <= 10)
         {
           waterState = true;
         }
+    }
+
+  //Monitoring myTDS and Setting up the Nutrition
+  myTDS.setTemperature(temperature);
+  myTDS.update();
+  tdsValue = myTDS.getTdsValue();
+  if (waterLevel == 100 && tdsValue < 1000 && waterState == false)
+    {
+      digitalWrite(nutritionValve, HIGH);
+      digitalWrite(relayPump2, HIGH);
+    }
+  else
+    {
+      digitalWrite(relayPump2, LOW);
+      digitalWrite(nutritionValve, LOW);
     }
   
   //MistMaker and DHT Set-Up
@@ -66,6 +105,13 @@ void loop() {
     {
       digitalWrite(relayMist, LOW);
     }
+
+  //Monitoring Soil pH
+  readPH = analogRead(pHPin);
+  soilPH = (-0.0693 * readPH) + 7.3855;
+
+  //Monitoring Light Intensity
+  readLux = luxMeter.readLightLevel();
   
   //WaterPump for Hydroponics Set-Up
   readSoil = analogRead(soilPin);
@@ -80,7 +126,7 @@ void loop() {
     }
 }
 
-
+//Ultrasonic Function
 int getDistance() {
 
   long duration = 0;
